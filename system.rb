eae4c97fbda0148235c3bba12262404d9ff53b35
Andrey Zax <andrey.zax@gmail.com>
2018-04-22 22:46:49 +0300

    Start working on virtual system objects
    Implement base MemoryArea class

---
+++ system.rb
@@ -0,0 +1,37 @@
+module System
+  STEP_SECONDS = 1.0
+
+  attr_reader: memory_map
+  @@memory_map = [] # Array of MemoryAreas
+
+  def System::memory_area_overlaps?(memory_area1, memory_area2)
+    if memory_area1.start >= memory_area2.start and memory_area1.start <= memory_area2.last
+      return true
+    elsif memory_area1.last >= memory_area2.start and memory_area1.last <= memory_area2.last
+      return true
+    elsif memory_area1.start <= memory_area2.start and memory_area1.last >= memory_area2.last
+      return true
+    else
+      return false
+    end
+  end
+
+  class MemoryArea
+
+    attr_reader :start
+    attr_reader :size
+
+    def initialize(start: 0, size: 0)
+      @start = start
+      @size = size
+    end
+
+    def last
+      return start + size - 1
+    end
+
+    def []
+      raise "Can't address locations in base MemoryArea class"
+    end
+  end
+end
