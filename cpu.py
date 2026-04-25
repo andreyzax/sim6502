c96087870212d77b8c48447e92a57de2889559fc
Andrey Zax <andrey.zax@gmail.com>
2026-04-25 14:00:43 +0300

    Add temp trap handling code

--- cpu.py
+++ cpu.py
@@ -722,4 +722,13 @@ class CPU:
                 self.step()
         except CPUTrap as ctx:
             # This all should probably be a callable passed in to to run()
-            print(f"Execution stopped:\n {ctx}")
+            print(f"""Execution stopped:
+                pc=0x{ctx.cpu.pc:X}
+                s=0x{ctx.cpu.s:X}
+                a=0x{ctx.cpu.a:X},x=0x{ctx.cpu.x:X},y=0x{ctx.cpu.y:X}
+                flags:   NV-BDIZC
+                         {"#" if ctx.cpu.p.negative else " "}{"#" if ctx.cpu.p.overflow else " "}##{"#" if ctx.cpu.p.decimal else " "}{"#" if ctx.cpu.p.interrupt_disable else " "}{"#" if ctx.cpu.p.zero else " "}{"#" if ctx.cpu.p.carry else " "}
+                """)
+
+            # print("Memory Dump:\n--------------------------------------")
+            # print(ctx.cpu.memory)
