90395e420a421b93ca0a27a54bc39c8392f4d6b8
Andrey Zax <andrey.zax@gmail.com>
2026-04-25 21:07:58 +0300

    Add addtional tests to increase test covrage for the memory module to 100%

--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,3 +1,4 @@
+from operator import add
 import pytest
 
 from memory import MemoryMap, RamSegment, PAGE_NR, PAGE_SIZE, RomSegment
@@ -25,6 +26,11 @@ def test_memory_map_simple_allocation(simple_memory_map: MemoryMap):
     mm = simple_memory_map
 
     assert len(mm._memory_map) == 2
+
+    for address in mm:
+        assert address == 0x0 or address == 0xFF or address == 0xEF
+
+    _ = mm.dump()  # This is a little dumb but hey, the things I do for coverage
 
 
 def test_disjoint_mm_allocation(disjoint_memory_map: MemoryMap):
@@ -70,8 +76,21 @@ def test_ram_segment(simple_memory_map: MemoryMap):
         mem_seg[0xFF] = 0x42
         assert mem_seg[0xFF] == 0x42
 
+        assert True == (RamSegment(0x100, 256) in mem_seg)
+        assert False == (RamSegment(0xFF00, 256) in mem_seg)
+        assert False == (0xA00 in mem_seg)
+        assert True == (0x100 in mem_seg)
 
-def test_rom_segment(simple_memory_map: MemoryMap):
+        mem_seg2 = RamSegment(0, 2560)
+        mem_seg3 = RamSegment(2560, 100)
+        mem_seg4 = RamSegment(0, 100)
+        assert True == (mem_seg == mem_seg2)
+        assert False == (mem_seg == mem_seg3)
+        assert True == (mem_seg == mem_seg4)
+
+
+def test_rom_segment(simple_memory_map: MemoryMap, mocker):
+    mock_file = mocker.patch("builtins.open", mocker.mock_open(read_data="\xca" * (64 * 1024)))
     base = 0xFF00
     size = 256
     mm = simple_memory_map
@@ -120,6 +139,15 @@ def test_rom_segment(simple_memory_map: MemoryMap):
         with pytest.raises(IndexError):
             _ = rom_seg[0x6400 + 128]
 
+        rom_seg2 = RomSegment(0x0, binary_file_source=open("dummy.bin", "rb"))
+        assert len(rom_seg2._backing_store) == 64 * 1024
+
+        rom_seg3 = RomSegment.from_binary_file(0x0, "dummy.bin")
+        assert len(rom_seg3._backing_store) == 64 * 1024
+
+        rom_seg4 = RomSegment(0x100, size=0xFF, bytes_source=b"\x40" * 0x80)
+        assert len(rom_seg4._backing_store) == 0xFF
+
 
 def test_simple_memory_map_access(simple_memory_map: MemoryMap):
     mm = simple_memory_map
@@ -131,6 +159,9 @@ def test_simple_memory_map_access(simple_memory_map: MemoryMap):
     assert mm[0xEC] == 0x42
 
     assert mm[0x0A00] == 0xFF
+
+    with pytest.raises(ValueError):
+        mm[0xEC] = 0x4242
 
     mm[0x0000] = 1
     mm[0x0100] = 2
@@ -177,6 +208,12 @@ def test_simple_memory_map_access(simple_memory_map: MemoryMap):
     assert mm[0xFF00:0x10002] == b"\xef" * 256
     with pytest.raises(ValueError):
         assert mm[0x10000] == 0xEF
+
+    with pytest.raises(ValueError):
+        _ = mm["foo"] = 42  # type: ignore
+
+    with pytest.raises(ValueError):
+        _ = mm[42] = "F"  # type: ignore
 
 
 def test_disjoint_memory_map_access(disjoint_memory_map):
@@ -230,3 +267,60 @@ def test_disjoint_memory_map_access(disjoint_memory_map):
         mm[0xFE00:0xFE05] = b"form"  # We don't support python's standard delete and replace semantics.
         # (what would that even look like for a fixed sized memory array)
         # If the slice is larger then the source data we raise an exception
+
+
+def test_ram_segment_errors():
+    with pytest.raises(ValueError):
+        _ = RamSegment(0, 0x10001)
+
+    with pytest.raises(ValueError):
+        _ = RamSegment(1, 0x10000)
+
+    ram_seg1 = RamSegment(0, 0x100)
+
+    with pytest.raises(TypeError):
+        "a" in ram_seg1  # type: ignore
+
+    with pytest.raises(TypeError):
+        4 & ram_seg1  # type: ignore
+
+    with pytest.raises(TypeError):
+        4 < ram_seg1  # type: ignore
+
+    # with pytest.raises(TypeError):
+    assert False == (ram_seg1 == 4)
+
+    with pytest.raises(ValueError):
+        ram_seg1[0x10] = 256
+
+
+def test_rom_segment_errors(mocker):
+    bad_mock_file = mocker.mock_open(read_data="\xca" * (64 * 1024 + 1))
+    good_mock_file = mocker.mock_open(read_data="\xca" * (64 * 1024))
+
+    def open_side_effect(path, *args, **kwargs):
+        if path == "too_big.bin":
+            return bad_mock_file()
+        if path == "dummy.bin":
+            return good_mock_file()
+
+        return mocker.DEFAULT
+
+    mocker.patch("builtins.open", side_effect=open_side_effect)
+
+    with pytest.raises(ValueError):
+        _ = RomSegment(0x8000, bytes_source=b"AAAAA", binary_file_source=open("too_big.bin", "rb"))
+
+    with pytest.raises(RuntimeError):
+        _ = RomSegment(0x8000, binary_file_source=open("too_big.bin", "rb"))
+
+    with pytest.raises(ValueError):
+        _ = RomSegment(0x8000, size=64 * 1024 - 1, binary_file_source=open("dummy.bin", "rb"))
+
+
+def test_memory_map_allocation_errors():
+    with pytest.raises(ValueError):
+        _ = MemoryMap(RamSegment(0, 256), RamSegment(256, 256), "what?!")  # type: ignore
+
+    with pytest.raises(RuntimeError):
+        _ = MemoryMap(RamSegment(0, 256), RomSegment.from_bytes(100, b"foooooo"))
