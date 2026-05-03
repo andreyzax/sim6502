b075d57f2c4b8f7350b66410ca0be9c147460db9
Andrey Zax <andrey.zax@gmail.com>
2026-05-03 10:08:03 +0300

    Remove run() method from CPU class
    CPU should not be repsonislbe for runtime control, move this to
    dedicated runtime controller

--- cpu.py
+++ cpu.py
@@ -14,8 +14,6 @@ Arithmetic & integer representation note:
 """
 
 import copy
-import itertools
-import time
 from dataclasses import dataclass
 from io import BufferedIOBase
 from typing import Callable
@@ -764,27 +762,3 @@ class CPU:
         """Execute one instruction from the current PC location and advance the PC to the next instruction."""
         ins = self._decode()
         self.execute_instruction(ins)
-
-    def run(self) -> None:
-        """Start the cpu's run loop, we only stop due to exceptions."""
-        counter = itertools.count()
-        runtime = 0
-        try:
-            for i in counter:
-                if config.enable_runtime_perf_metrics:
-                    start = time.perf_counter_ns()
-
-                if i % 10000 == 0:
-                    self.memory.poll_hardware()
-                self.step()
-                if config.enable_runtime_perf_metrics:
-                    runtime = runtime + (time.perf_counter_ns() - start)  # pyright: ignore [ reportPossiblyUnboundVariable ]
-        except KeyboardInterrupt:
-            print("\nExecution stopped.")
-            if config.enable_runtime_perf_metrics:
-                instructions = next(counter)
-                ips = round(instructions / (runtime / 10**9))
-                avg_ins_time = runtime / instructions / 1000  # Show in microseconds
-                print(
-                    f"Runtime: {runtime / 10**9} seconds, instructions: {instructions}, ips: {ips:,}, average instruction time: {avg_ins_time:.3f}us"
-                )
